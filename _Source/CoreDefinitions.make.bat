b0d6c0f5100d694b7e6a111c92df83382ad241cb
Stan H <deindall@outlook.com>
2017-06-24 18:25:19 +0200

    added GetWrapper for simple & small getters

--- _Source/CoreDefinitions.make.bat
+++ _Source/CoreDefinitions.make.bat
@@ -6,6 +6,7 @@ type bits\FE8Check.bit.event
 
 bin2ea --to-stdout -short "asm\core\GetterWrapper.bin" -define "rGetterWrapper(aplprRoutineList)" -after "POIN prCallSequence aplprRoutineList"
 bin2ea --to-stdout -short "asm\core\BCWrapper.bin"     -define "rBCWrapper"
+bin2ea --to-stdout -short "asm\core\GetWrapper.bin"    -define "rGetWrapper"
 echo:
 
 type bits\CoreHelpers.bit.event
---
+++ _Source/asm/core/GetWrapper.s
@@ -0,0 +1,7 @@
+.thumb
+
+GetWrapper:
+	mov r2, r1 @ Moving Subject
+	mov r1, r0 @ Moving Argument
+	mov r0, #0 @ Stat starts at 0
+	nop
